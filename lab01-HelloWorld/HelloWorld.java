752e398d363fc11d5e56dacf7a8d2fad5262db8c
loucopoulosc <col217@lehigh.edu>
2014-08-29 14:55:15 +0000

    updated HelloWorld

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,29 @@
+//
+//Constantine Loucopoulos
+//Lab01
+//Hello World Java Program
+//
+//  First compile the program
+//      Javac HelloWorld.java
+//  Run the program
+//      Java HelloWorld//
+
+//  Define a Class
+public class HelloWorld{
+    
+//  Add main method
+    public static void main(String[] args)  {
+        
+//  Print the statement with box around it
+    System.out.println("#########################");
+    System.out.println("#########################");
+    System.out.println("#                       #");
+    System.out.println("#   Hello World 2014!!  #");
+    System.out.println("#                       #");
+    System.out.println("#########################");
+    System.out.println("#########################");
+    System.out.println("Test");
+    
+    //Test/TestTest
+    }
+}
