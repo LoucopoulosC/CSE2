d37cc400430c26286ed531cdf0406b0ed057cdfb
loucopoulosc <col217@lehigh.edu>
2014-09-03 02:43:53 +0000

    updatedWelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,32 @@
+//
+//Constantine Loucopoulos
+//Hw01
+//Welcome Class Java Program
+//
+//  First compile the program
+//      Javac WelcomeClass.java
+//  Run the program
+//      Java WelcomeClass//
+
+//  Define a Class
+public class WelcomeClass{
+    
+//  Add main method
+    public static void main(String[] args)  {
+    
+//  Print the welcome message and your signature inside the border
+//  Then write short autobiographical message below
+    System.out.println("   -----------");
+    System.out.println("   | WELCOME |");
+    System.out.println("   -----------");
+    System.out.println("  ^  ^  ^  ^  ^  ^");
+    System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\ ");
+    System.out.println("|-C--O--L--2--1--7-|");
+    System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+    System.out.println("  v  v  v  v  v  v");
+    System.out.println("This summer I went to Greece for 3 weeks to visit my family.");
+    System.out.println("We ate lots of delicious food.");
+    
+    //Test/TestTest
+    }
+}
