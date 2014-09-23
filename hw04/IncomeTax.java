02d136019d2a3f9e85e77f4a357c983ababdf018
loucopoulosc <col217@lehigh.edu>
2014-09-23 23:37:41 +0000

    Updated BigMacAgain

---
+++ hw04/IncomeTax.java
@@ -0,0 +1,16 @@
+//
+//Constantine Loucopoulos
+//Lab 03
+//IncomeTax Java Program
+//
+//  First compile the program
+//      Javac IncomeTax.java
+//  Run the program
+//      Java IncomeTax//
+
+
+
+if (nBigMacs<0) {
+            System.out.print("You did not enter an int > 0.");
+            return; //  Terminates program
+            }
---
+++ lab04-BigMacAgain/BigMacAgain.java
@@ -0,0 +1,51 @@
+//
+//Constantine Loucopoulos
+//Lab 03
+//BigMac Java Program
+//
+//  First compile the program
+//      Javac BigMac.java
+//  Run the program
+//      Java BigMac//
+
+
+
+//  Import Scanner
+import java.util.Scanner;
+
+//  Define a Class
+public class BigMacAgain{
+    
+    //  Add main method
+    public static void main(String[] args)  {
+
+        //  Declare instance of scanner object
+        Scanner myScanner;
+
+        //  Call Scanner constructor
+        myScanner=new Scanner(System.in);
+
+        //  Accept input by prompting user for number of Big Macs
+        System.out.print("Enter the number of Big Macs: ");
+
+        int nBigMacs=myScanner.nextInt();
+        
+         //  Constants
+        double costBigMac$=2.22;
+        double costFries$=2.15;
+        double totalCostBigMac$, totalCostFries$;
+
+        totalCostBigMac$=nBigMacs*costBigMac$;
+        
+        
+        if (nBigMacs>0) {
+            System.out.print("You ordered "+nBigMacs+" Big Macs for a cost of: "+nBigMacs+" x $2.22 = "+totalCostBigMac$);}
+        
+        
+            
+        else {System.out.print("You did not enter an int.");
+        return; //  Terminates program
+        }
+            
+    }   //  End of main method
+}   //  End of classs
